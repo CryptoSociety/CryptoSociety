8bb5537e2d438c2c6b1d132a2b586fe5c549a7f0
N Duke <duke9289@icloud.com>
2017-01-13 15:45:22 -0600

    initial commit

---
+++ src/main/java/com/codeup/CryptoSocietyApplication.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoSocietyApplication.class, args);
+	}
+}
