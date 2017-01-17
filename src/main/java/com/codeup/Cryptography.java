ff80b1adb69f90df5b5af428707377321569f959
N Duke <duke9289@icloud.com>
2017-01-17 15:53:54 -0600

    Corrected caesar function so letters loop around at Z
    Decided not to shift numbers, as traditional cipher did not change numbers.

--- src/main/java/com/codeup/Cryptography.java
+++ src/main/java/com/codeup/Cryptography.java
@@ -4,27 +4,27 @@ package com.codeup;
  * Created by Duke on 1/17/17.
  */
 public class Cryptography {
-    public void caesar(String plaintext, int shift, boolean punctuation) {
+    public String caesar(String plaintext, int shift, boolean punctuation) {
         plaintext = prepare(plaintext, punctuation);
         StringBuilder ciphertext = new StringBuilder(plaintext);
         for (int i = 0; i < ciphertext.length(); i++){
             char letter = ciphertext.charAt(i);
-            if (isAlphnumeric(letter)){
-                ciphertext.setCharAt(i, (char)(letter + shift));
+            if (Character.isLetter(letter)){
+                ciphertext.setCharAt(i, (char)((letter + shift - (int)'A') % 26 + (int)'A'));
             }
         }
-        System.out.println(ciphertext);
+        return ciphertext.toString();
     }
 
-    public void caesarDecypt(String ciphertext, int shift) {
+    public String caesarDecypt(String ciphertext, int shift) {
         StringBuilder plaintext = new StringBuilder(ciphertext);
         for (int i = 0; i < plaintext.length(); i++){
             char letter = plaintext.charAt(i);
-            if (isAlphnumeric(letter)){
-                plaintext.setCharAt(i, (char)(letter - shift));
+            if (Character.isLetter(letter)){
+                plaintext.setCharAt(i, (char)((letter - shift + (int)'A') % 26 + (int)'A'));
             }
         }
-        System.out.println(plaintext);
+        return plaintext.toString();
     }
 
     private String prepare(String input, boolean punctuation) {
