2e3e30b8be52cb46a0639395d69233310ece818c
N Duke <duke9289@icloud.com>
2017-01-17 15:23:42 -0600

    Build Caesar shift encryption and decription function
    Currently does not wrap around at Z, goes into symbols

--- src/main/java/com/codeup/CryptoTestApp.java
+++ src/main/java/com/codeup/CryptoTestApp.java
@@ -4,4 +4,19 @@ package com.codeup;
  * Created by Duke on 1/17/17.
  */
 public class CryptoTestApp {
+    public static void main(String[] args) {
+        Cryptography c = new Cryptography();
+        c.caesar("Dear John, I'm testing a letter!  Isn't that great?  Now, is this a shift of 1, 2, 3, 4, 5... XOXO " +
+                        "-Kathy",
+                100,
+                true);
+        c.caesar("Dear John, I'm testing a letter!  Isn't that great?  Now, is this a shift of 1, 2, 3, 4, 5...  XOXO" +
+                " -Kathy",
+                100,
+                false);
+        c.caesarDecypt("EFBS KPIO, J'N UFTUJOH B MFUUFS!  JTO'U UIBU HSFBU?  OPX, JT UIJT B TIJGU PG 2, 3, 4, 5, 6..." +
+                " YPYP -LBUIZ",
+                100);
+        c.caesarDecypt("EFBSKPIOJNUFTUJOHBMFUUFSJTOUUIBUHSFBUOPXJTUIJTBTIJGUPG23456YPYPLBUIZ", 100);
+    }
 }
--- src/main/java/com/codeup/Cryptography.java
+++ src/main/java/com/codeup/Cryptography.java
@@ -4,4 +4,37 @@ package com.codeup;
  * Created by Duke on 1/17/17.
  */
 public class Cryptography {
+    public void caesar(String plaintext, int shift, boolean punctuation) {
+        plaintext = prepare(plaintext, punctuation);
+        StringBuilder ciphertext = new StringBuilder(plaintext);
+        for (int i = 0; i < ciphertext.length(); i++){
+            char letter = ciphertext.charAt(i);
+            if (isAlphnumeric(letter)){
+                ciphertext.setCharAt(i, (char)(letter + shift));
+            }
+        }
+        System.out.println(ciphertext);
+    }
+
+    public void caesarDecypt(String ciphertext, int shift) {
+        StringBuilder plaintext = new StringBuilder(ciphertext);
+        for (int i = 0; i < plaintext.length(); i++){
+            char letter = plaintext.charAt(i);
+            if (isAlphnumeric(letter)){
+                plaintext.setCharAt(i, (char)(letter - shift));
+            }
+        }
+        System.out.println(plaintext);
+    }
+
+    private String prepare(String input, boolean punctuation) {
+        input = input.toUpperCase();
+        if (punctuation)
+            return input;
+        return input.replaceAll("[^A-Z0-9]", "");
+    }
+
+    private boolean isAlphnumeric(char c){
+        return (Character.isLetter(c) || Character.isDigit(c));
+    }
 }
