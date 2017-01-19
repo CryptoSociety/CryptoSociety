daab7f84ae2aa70da0758f7d41405ddcd788cfe3
N Duke <duke9289@icloud.com>
2017-01-19 16:43:24 -0600

    build atbash ajax partial

--- src/main/java/com/codeup/auth/WorkbenchController.java
+++ src/main/java/com/codeup/auth/WorkbenchController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.*;
  * Created by Duke on 1/19/17.
  */
 @Controller
-public class CaesarController {
+@RequestMapping("/workbench")
+public class WorkbenchController {
     Cryptography c = new Cryptography();
-    @GetMapping("/caesartool")
+
+    @GetMapping("/caesar")
     public String makeCaesarTool(Model m){
-        m.addAttribute("crypto", c);
-        return "caesar";
+        m.addAttribute("system", "caesar");
+        return "tool";
     }
 
     @RequestMapping(value = "/caesartool.json", method = RequestMethod.GET, produces = "application/json")
@@ -33,6 +35,18 @@ public class CaesarController {
             @RequestParam("ciphertext") String ciphertext,
             @RequestParam("shift") int shift){
         String output =  c.caesarDecypt(ciphertext, shift);
+        return output;
+    }
+
+    @GetMapping("/atbash")
+    public String makeAtbashTool(Model m){
+        m.addAttribute("system", "atbash");
+        return "tool";
+    }
+
+    @RequestMapping(value = "/atbashtool.json", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody String getAtbash(@RequestParam("input") String input){
+        String output = c.atbash(input, true);
         return output;
     }
 }
--- src/main/resources/static/js/caesar.js
+++ src/main/resources/static/js/caesar.js
@@ -2,3 +2,37 @@
  * Created by Duke on 1/19/17.
  */
 'use strict';
+
+$(document).ready(function() {
+    var $plaintext = $('#plaintextBox'),
+        $ciphertext = $('#ciphertextBox'),
+        $encrypt = $('#encrypt'),
+        $decrypt = $('#decrypt'),
+        $shift = $('#shiftAmount');
+    $encrypt.click(function () {
+        $.ajax({url: "/workbench/caesartool.json",
+            type: 'GET',
+            dataType: 'text',
+            data: {plaintext: $plaintext.val(),
+                shift: $shift.val(),
+                punctuation: true}
+        }).done(function (r) {
+            $ciphertext.val(r)
+        }).fail(function (e) {
+            console.log(e);
+        });
+    });
+    $decrypt.click(function () {
+        $.ajax({url: "/workbench/caesartool/decrypt.json",
+            type: 'GET',
+            dataType: 'text',
+            data: {ciphertext: $ciphertext.val(),
+                shift: $shift.val(),
+                punctuation: true}
+        }).done(function (r) {
+            $plaintext.val(r)
+        }).fail(function (e) {
+            console.log(e);
+        });
+    });
+});
