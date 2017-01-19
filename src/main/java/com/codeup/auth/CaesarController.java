b0d51b24d9d5946d422fe20fd64cb528fc968164
N Duke <duke9289@icloud.com>
2017-01-19 14:18:59 -0600

    build test form and controller as proof of concept for ajax

--- src/main/java/com/codeup/auth/CaesarController.java
+++ src/main/java/com/codeup/auth/CaesarController.java
@@ -1,7 +1,40 @@
 package com.codeup.auth;
 
+import com.codeup.Cryptography;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
 /**
  * Created by Duke on 1/19/17.
  */
+@Controller
 public class CaesarController {
+    Cryptography c = new Cryptography();
+    @GetMapping("/caesartool")
+    public String makeCaesarTool(Model m){
+        m.addAttribute("crypto", c);
+        return "caesar";
+    }
+
+    @RequestMapping(value = "/caesartool.json", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    String getCaesarcipher(
+            @RequestParam("plaintext") String plaintext,
+            @RequestParam("shift") String shift,
+            @RequestParam("punctuation") boolean punctuation){
+        String output =  c.caesar(plaintext, Integer.parseInt(shift), punctuation);
+        return output;
+    }
+    @RequestMapping(value = "/caesartool/decrypt.json", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    String getCaesardecrypt(
+            @RequestParam("ciphertext") String ciphertext,
+            @RequestParam("shift") String shift){
+        String output =  c.caesarDecypt(ciphertext, Integer.parseInt(shift));
+        return output;
+    }
 }
