53cd6e5ed5c4e5b614918bcb2a78e6a6e1797972
N Duke <duke9289@icloud.com>
2017-01-20 14:45:49 -0600

    actually tests user solution against crypto

--- src/main/java/com/codeup/CryptoController.java
+++ src/main/java/com/codeup/CryptoController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -151,10 +148,10 @@ public class CryptoController {
     }
 
     @PostMapping("{id}/solve")
-    public String solveCrypto(@PathVariable long id){
+    public String solveCrypto(@PathVariable long id, @RequestParam("solution") String solution){
 
 // TODO: Implement actual check for correct-ness
-        boolean cryptoIsCorrect = true;
+        boolean cryptoIsCorrect = (cryptosRepo.findOne(id).getSolution().equals(solution));
 //------------------------------------------------------
         if(cryptoIsCorrect){
             Crypto crypto = cryptosRepo.findOne(id);
