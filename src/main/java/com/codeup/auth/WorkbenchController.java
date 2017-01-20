d48b780b001a57214c6b27e200e0bf6f9f23ccc3
N Duke <duke9289@icloud.com>
2017-01-20 09:20:55 -0600

    build ajax function for railfence cipher

--- src/main/java/com/codeup/auth/WorkbenchController.java
+++ src/main/java/com/codeup/auth/WorkbenchController.java
@@ -60,8 +60,31 @@ public class WorkbenchController {
     public @ResponseBody String encryptKamaSutra(
             @RequestParam("input") String input,
             @RequestParam("key") String key) throws Exception {
-        System.out.println(key);
         String output = c.kamasutra(input, key.toUpperCase().toCharArray(), true);
+        return output;
+    }
+
+    @GetMapping("/railfence")
+    public String makeRailfenceTool(Model m){
+        m.addAttribute("system", "railfence");
+        return "tool";
+    }
+
+    @RequestMapping(value = "/railfencetool.json", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody String encryptRailfence(
+            @RequestParam("plaintext") String plaintext,
+            @RequestParam("rails") int rails
+    ){
+        String output = c.railfence(plaintext, rails, true);
+        return output;
+    }
+
+    @RequestMapping(value = "/railfencetool/decrypt.json", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody String decryptRailfence(
+            @RequestParam("ciphertext") String ciphertext,
+            @RequestParam("rails") String rails
+    ){
+        String output = c.railfenceDecrypt(ciphertext, Integer.parseInt(rails));
         return output;
     }
 
---
+++ src/main/resources/static/js/railfence.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Duke on 1/20/17.
+ */
+'use strict';
+$(document).ready(function() {
+    var $plaintext = $('#plaintextBox'),
+        $ciphertext = $('#ciphertextBox'),
+        $encrypt = $('#encrypt'),
+        $decrypt = $('#decrypt'),
+        $rails = $('#numberOfRails');
+    $encrypt.click(function () {
+        $.ajax({
+            url: "/workbench/railfencetool.json",
+            type: 'GET',
+            dataType: 'text',
+            data: {
+                plaintext: $plaintext.val(),
+                rails: $rails.val()
+            }
+        }).done(function (r) {
+            $ciphertext.val(r)
+        }).fail(function (e) {
+            console.log(e);
+        });
+    });
+    $decrypt.click(function () {
+        $.ajax({
+            url: "/workbench/railfencetool/decrypt.json",
+            type: 'GET',
+            dataType: 'text',
+            data: {
+                ciphertext: $ciphertext.val(),
+                rails: $rails.val()
+            }
+        }).done(function (r) {
+            $plaintext.val(r)
+        }).fail(function (e) {
+            console.log(e);
+        });
+    });
+})
