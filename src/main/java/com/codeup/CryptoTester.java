101cc8d06fa660ec728e6fd353b47836c9d56344
N Duke <duke9289@icloud.com>
2017-01-20 13:42:15 -0600

    initial commit

---
+++ src/main/java/com/codeup/CryptoTester.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import com.codeup.models.Crypto;
+
+/**
+ * Created by Duke on 1/20/17.
+ */
+public class CryptoTester {
+    public static boolean test(Crypto crypto){
+        return true;
+    }
+}
