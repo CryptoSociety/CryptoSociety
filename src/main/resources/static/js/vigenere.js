802e83123b01af3c4fb94385cce0366fee640fca
N Duke <duke9289@icloud.com>
2017-01-21 22:40:50 -0600

    add frontend verification to vigenere

--- src/main/resources/static/js/vigenere.js
+++ src/main/resources/static/js/vigenere.js
@@ -7,35 +7,52 @@ $(document).ready(function() {
         $ciphertext = $('#ciphertextBox'),
         $encrypt = $('#encrypt'),
         $decrypt = $('#decrypt'),
-        $keyword = $('#keyword');
+        $keyword = $('#keyword'),
+        $error = $('#error');
     $encrypt.click(function () {
-        $.ajax({
-            url: "/workbench/vigeneretool.json",
-            type: 'GET',
-            dataType: 'text',
-            data: {
-                plaintext: $plaintext.val(),
-                keyword: $keyword.val()
-            }
-        }).done(function (r) {
-            $ciphertext.val(r)
-        }).fail(function (e) {
-            console.log(e);
-        });
+        if(!allLetters($keyword.val()))
+            $error.text("The keyword must not contain numbers or special symbols/characters.");
+        else {
+            $.ajax({
+                url: "/workbench/vigeneretool.json",
+                type: 'GET',
+                dataType: 'text',
+                data: {
+                    plaintext: $plaintext.val(),
+                    keyword: $keyword.val()
+                }
+            }).done(function (r) {
+                $ciphertext.val(r)
+            }).fail(function (e) {
+                console.log(e);
+            });
+        }
     });
     $decrypt.click(function () {
-        $.ajax({
-            url: "/workbench/vigeneretool/decrypt.json",
-            type: 'GET',
-            dataType: 'text',
-            data: {
-                ciphertext: $ciphertext.val(),
-                keyword: $keyword.val()
-            }
-        }).done(function (r) {
-            $plaintext.val(r)
-        }).fail(function (e) {
-            console.log(e);
-        });
+        if(!allLetters($keyword.val()))
+            $error.text("The keyword must not contain numbers or special symbols/characters.");
+        else {
+            $.ajax({
+                url: "/workbench/vigeneretool/decrypt.json",
+                type: 'GET',
+                dataType: 'text',
+                data: {
+                    ciphertext: $ciphertext.val(),
+                    keyword: $keyword.val()
+                }
+            }).done(function (r) {
+                $plaintext.val(r)
+            }).fail(function (e) {
+                console.log(e);
+            });
+        }
     });
+    function allLetters(input) {
+        var result = true;
+        for (var i = 0; i < input.length; i++){
+            if (input.charAt(i).toUpperCase() == input.charAt(i).toLowerCase())
+                result = false;
+        }
+        return result;
+    }
 })
